47fe75e7752e410f675a8dcee320bb052f0252dd
dscanlonpa <dscanlonpa@gmail.com>
2015-02-24 19:10:53 -0500

    Re-wrote to consider arrays, null, and undefined.

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 var _ = require('underscore');
 
-module.exports = function extend(object, patch) { 
+module.exports = function patch(object, patch) { 
   _.each(patch, function(value, key) {
-    var increment = value['$inc'];
+    var increment = (_.isObject(value) ? value || {})['$inc'];
 
-    object[key] = (_.isObject(value)
-      ? (increment !== undefined
-        ? object[key] += increment
-        : extend(object[key], value))
-      : value);
+    // Unset key on object if value is null or undefined
+    if (_.isNull(value) || _.isUndefined(value)) {
+      return delete object[key];
+    }
+
+    // Overwrite value of key prop on object with value 
+    // if value is array.
+    if (_.isArray(value)) {
+      return object[key] = value;
+    }
+
+    // If increment operator, increment (+=) value of 
+    // object's key prop with value.
+    if (increment) {
+      return object[key] += value;
+    }
+
+    // If value is an object, extend object's key prop 
+    // with value.
+    if (_.isObject(value)) {
+      return extend(object[key], value);
+    }
+
+    // Value is not null, undefined, an array, an increment 
+    // operator value, or an object.  Overwrite key prop on 
+    // object with value.
+    object[key] = value;
   });
 
   return object;
